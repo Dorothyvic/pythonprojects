a71db3a40ebaf51bc93a3f4fe628770f508f4724
Dorothyvic <65293909+Dorothyvic@users.noreply.github.com>
2020-05-18 15:16:47 +0300

    second commit

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
 name = input('What is your name? ')
 colour = input('What is your favourite colour? ')
 print(name + ' likes ' + colour)
+birth_year = int(input('Birth year: '))
+age = 2019 -birth_year
+pritn(age)
