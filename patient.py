fff6cc87448a6f7a93345813a43cb5727ecdc210
dorothy <dorothyvic24@gmail.com>
2020-05-19 10:38:18 +0300

    Added patient.py

---
+++ patient.py
@@ -0,0 +1,7 @@
+name = 'dorothy'
+if len(name) < 3:
+    print('Name must be atleast three characters')
+elif len(name) > 50:
+    print('Name can be a maximum of 50 characters')
+else:
+    print('Name looks good!')
