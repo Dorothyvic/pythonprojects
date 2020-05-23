46e4de72fac5804751ea33a130018aeb71414623
dorothy <dorothyvic24@gmail.com>
2020-05-23 09:47:34 +0300

    modified def.py

--- def.py
+++ def.py
@@ -1,15 +1,15 @@
-def phone_converter(phone):
+def phone_converter():
     numbers = {
             '1': 'one',
             '2': 'two',
             '3': 'three',
             '4': 'four'
             }
-    for number in phone:
-        return numbers.get(number, '!') + ' '
+    for number in num:
+        print(numbers.get(number, '!'), end=' ')
 
 
-phone = input('Phone: ')
-print(phone_converter(phone))
+num = input('Phone: ')
+phone_converter()
 
 
